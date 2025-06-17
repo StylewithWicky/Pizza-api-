876d32fc03d7c5604b8c1300887c7cdc480932d1
Yazz <itsjustmaitai@gmail.com>
2025-06-17 19:25:46 +0300

    Added app.py

---
+++ server/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Local imports
+from .config import Config
+from .models import db
+from .controllers import register_routes
+
+# Initialize Flask extensions
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Load config
+    app.config.from_object(Config)
+
+    # Initialize database and migrations
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register all routes via blueprint registration function
+    register_routes(app)
+
+    return app
