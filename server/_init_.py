488d8f3e57535a6cdcd499e513912530b052f6bb
Yazz <itsjustmaitai@gmail.com>
2025-06-17 19:22:56 +0300

    Added _init_.py

---
+++ server/_init_.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from flask_migrate import Migrate
+
+db=SQLAlchemy
+migrate=Migrate()
+
+def app_create():
+    app=Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app,db)
+
+    from controllers import register_routes
+    register_routes(app)
+
+    return app
